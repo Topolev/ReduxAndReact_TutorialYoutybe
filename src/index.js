ed40bbe72a7de8dad365f806b388b92be676d060
Topolev <i.topolev.vladimir@gmail.com>
2017-03-05 14:05:34 +0300

    Lesson 02: https://www.youtube.com/watch?v=KCmFLI008dc&index=2&list=PLIcAMDxr6tprSzqKmfhDiW00GWbDcs8lE

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import {createStore} from "redux";
 
 
-function playList(state = [], action){
-    if (action.type == "ADD_TRACK"){
+function playList(state = [], action) {
+    if (action.type == "ADD_TRACK") {
         return [
             ...state,
             action.payload
@@ -15,9 +15,29 @@ function playList(state = [], action){
 const store = createStore(playList);
 
 
+store.dispatch({type: 'ADD_TRACK', payload: 'How get away the murder'});
+
+const addTrackButton = document.querySelectorAll(".addTrack")[0];
+const inputTrack = document.querySelectorAll(".trackInput")[0];
+
+addTrackButton.addEventListener('click', () => {
+    const newTrack = inputTrack.value;
+    store.dispatch({type: 'ADD_TRACK', payload: newTrack});
+});
+
+const ulPlayList = document.querySelectorAll(".playList")[0];
+
+
 store.subscribe(() => {
-    console.log(store.getState());
+    displayPlayList(store.getState(), ulPlayList);
 })
 
-store.dispatch({type: 'ADD_TRACK', payload: 'Switched at birth'});
-store.dispatch({type: 'ADD_TRACK', payload: 'How get away the murder'});
+function displayPlayList(playList, ulPlayList) {
+    ulPlayList.innerHTML = "";
+    playList.forEach(track => {
+        let li = document.createElement("li");
+        li.textContent = track;
+        ulPlayList.appendChild(li);
+    });
+    inputTrack.value = "";
+}
